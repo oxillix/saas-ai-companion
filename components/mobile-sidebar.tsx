eb9c01183647205181182f7708d52c98014467f6
Nicolas Arnouts <nicolas.arnouts@outlook.com>
2023-10-17 20:46:19 +0200

    isPro prop was missing in MobileSidebar

--- components/mobile-sidebar.tsx
+++ components/mobile-sidebar.tsx
@@ -2,15 +2,18 @@ import { Menu } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import Sidebar from "./sidebar";
 
+interface MobileSidebarProps {
+  isPro: boolean;
+}
 
-const MobileSidebar = () => {
+const MobileSidebar = ({ isPro }: MobileSidebarProps) => {
   return (
     <Sheet>
       <SheetTrigger className="md:hidden pr-4">
         <Menu />
       </SheetTrigger>
       <SheetContent side="left" className="p-0 bg-secondary pt-10 w-32">
-        <Sidebar />
+        <Sidebar isPro={isPro} />
       </SheetContent>
     </Sheet>
   );
